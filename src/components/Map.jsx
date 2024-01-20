62a2e2b68660d4fcf5ef2d49b75e58f85ab81fe2
Daniyal Bekinalkar <80331663+dbekinalkar@users.noreply.github.com>
2024-01-20 13:57:13 -0500

    Display map using react-simple-map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,9 @@
+import MapView from "./MapView";
+
 const Map = () => {
   return (
     <div className="w-full h-full min-h-96 flex items-center justify-center bg-white">
-      Map
+      <MapView /> 
     </div>
   );
 };
